2c5024c16f7eaaf3890a6ade0be67872909e843b
Kiera Allen <kieraaallen@gmail.com>
2020-06-08 12:42:57 -0600

    Change date in attribute test to offset, replace date instance variable with offset, add create_shift method and pass test

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -1,13 +1,14 @@
 class Decryptor
   attr_reader :message,
               :key,
+              :offset,
               :date,
               :character_set
 
   def initialize(message, key, date)
     @message = message
     @key = key
-    @date = date
+    @offset = create_offsets(date)
     @character_set = ("a".."z").to_a << " "
   end
 
@@ -16,4 +17,15 @@ class Decryptor
     squared_date.to_s[-4..-1]
   end
 
+  def create_shift(shift_type)
+    if shift_type == :A
+      key[0..1].to_i + offset[0].to_i
+    elsif shift_type == :B
+      key[1..2].to_i + offset[1].to_i
+    elsif shift_type == :C
+      key[2..3].to_i + offset[2].to_i
+    else
+      key[3..4].to_i + offset[3].to_i
+    end
+  end
 end
--- test/decryptor_test.rb
+++ test/decryptor_test.rb
@@ -17,11 +17,18 @@ class DecryptorTest < Minitest::Test
 
       assert_equal "keder ohulw", @decryptor.message
       assert_equal "02715", @decryptor.key
-      assert_equal "040895", @decryptor.date
+      assert_equal "1025", @decryptor.offset
       assert_equal expected, @decryptor.character_set
   end
 
   def test_it_can_create_offsets
     assert_equal "1025", @decryptor.create_offsets("040895")
+  end
+
+  def test_it_can_create_shift
+    assert_equal 3, @decryptor.create_shift(:A)
+    assert_equal 73, @decryptor.create_shift(:C)
+    assert_equal 27, @decryptor.create_shift(:B)
+    assert_equal 20, @decryptor.create_shift(:D)
   end
 end
